7d142f4d81774451b2f9ba2ff165fa7499598c6c
Maurice Yang <mauriceyangmy28@gmail.com>
2021-11-21 20:59:52 +0700

    Added storage.py for handling whitenoise

--- djangorestchat/settings.py
+++ djangorestchat/settings.py
@@ -130,6 +130,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_STORAGE = 'MB3PemilihanKetosSite.storage.WhiteNoiseStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
